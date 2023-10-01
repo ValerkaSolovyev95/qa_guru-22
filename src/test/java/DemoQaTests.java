4cd8d9183ec14121937b2715ca323d4434ea05dc
VA.Solovev <va.solovev@tinkoff.ru>
2023-10-01 21:51:35 +0300

    fix : add userTestData

--- src/test/java/DemoQaTests.java
+++ src/test/java/DemoQaTests.java
@@ -1,70 +1,51 @@
-import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 import pages.StudentsRegistrationFormPage;
-import utils.RandomUtils;
 
 import static pages.components.FillingForm.assertTitle;
 import static pages.components.FillingForm.verifyResult;
 
-public class DemoQaTests extends BaseTest{
-    private static final RandomUtils userData = new RandomUtils();
-    private static final Faker faker = new Faker();
-    private static final String URL_DEMO_QA = "automation-practice-form";
-    public static final String USER_NAME = faker.name().firstName();
-    public static final String USER_LAST_NAME = faker.name().lastName();
-    public static final String USER_EMAIL = faker.internet().emailAddress();
-    public static final String USER_PHONE = userData.getPhoneNumber();
-    public static final String DEFAULT_MONTH = userData.getMonth();
-    public static final String DEFAULT_YEAR = userData.getYear(1980, 2000);
-    public static final String DEFAULT_ADDRESS = faker.address().fullAddress();
-    public static final String DEFAULT_SUBJECT = "English";
-    public static final String DEFAULT_PICTURE = "msk.jpg";
-    public static final String DEFAULT_STATE = userData.getState();
-    public static final String DEFAULT_CITY = userData.getCity(DEFAULT_STATE);
-    public static final String TITLE_TEXT = "Thanks for submitting the form";
-    private static final String DEFAULT_DAY = userData.getDay();
-    public static final String SUBJECT_FIRST_LATTER = "e";
-    public static final String FILE_NAME = "msk.jpg";
-    public static final String DEFAULT_GENDER = userData.getGender();
+class DemoQaTests extends BaseTest{
     StudentsRegistrationFormPage studentsRegistrationFormPage = new StudentsRegistrationFormPage();
 
     @Test
     void fieldsCheckTest() {
-        studentsRegistrationFormPage.openPage(URL_DEMO_QA)
-                .setFirstName(USER_NAME)
-                .setLastName(USER_LAST_NAME)
-                .setEmail(USER_EMAIL)
-                .selectGender(DEFAULT_GENDER)
-                .setPhone(USER_PHONE)
-                .setCalendar(DEFAULT_MONTH, DEFAULT_YEAR, DEFAULT_DAY)
-                .selectSubject(SUBJECT_FIRST_LATTER)
-                .selectHobby(userData.getHobby())
-                .uploadPhoto(FILE_NAME)
-                .setCurrentAddress(DEFAULT_ADDRESS)
-                .setState(DEFAULT_STATE)
-                .setCity(DEFAULT_CITY)
+        UserTestData userTestData = new UserTestData();
+        studentsRegistrationFormPage.openPage(UserTestData.AUTOMATION_PRACTICE_FORM)
+                .setFirstName(userTestData.firstName)
+                .setLastName(userTestData.lastName)
+                .setEmail(userTestData.emailAddress)
+                .selectGender(userTestData.gender)
+                .setPhone(userTestData.phoneNumber)
+                .setCalendar(userTestData.userDataMonth, userTestData.dataYear, userTestData.dataDay)
+                .selectSubject(UserTestData.subjectFirstLatter)
+                .selectHobby(userTestData.hobby)
+                .uploadPhoto(userTestData.image)
+                .setCurrentAddress(userTestData.fullAddress)
+                .setState(userTestData.userDataState)
+                .setCity(userTestData.userDataCity)
                 .submitButtonClick();
-        verifyResult(USER_NAME);
-        verifyResult(USER_LAST_NAME);
-        verifyResult(USER_EMAIL);
-        verifyResult(DEFAULT_GENDER);
-        verifyResult(USER_PHONE);
-        verifyResult(DEFAULT_SUBJECT);
-        verifyResult(DEFAULT_PICTURE);
-        verifyResult(DEFAULT_ADDRESS);
-        verifyResult(DEFAULT_STATE);
-        verifyResult(DEFAULT_CITY);
+        verifyResult(userTestData.firstName);
+        verifyResult(userTestData.lastName);
+        verifyResult(userTestData.emailAddress);
+        verifyResult(userTestData.gender);
+        verifyResult(userTestData.phoneNumber);
+        verifyResult(userTestData.defaultSubject);
+        verifyResult(userTestData.image);
+        verifyResult(userTestData.fullAddress);
+        verifyResult(userTestData.userDataState);
+        verifyResult(userTestData.userDataCity);
     }
 
     @Test
     void checkMandatoryFields() {
-        studentsRegistrationFormPage.openPage(URL_DEMO_QA)
-                .setFirstName(USER_NAME)
-                .setLastName(USER_LAST_NAME)
-                .setEmail(USER_EMAIL)
-                .selectGender(DEFAULT_GENDER)
-                .setPhone(USER_PHONE)
+        UserTestData userTestData = new UserTestData();
+        studentsRegistrationFormPage.openPage(UserTestData.AUTOMATION_PRACTICE_FORM)
+                .setFirstName(userTestData.firstName)
+                .setLastName(userTestData.lastName)
+                .setEmail(userTestData.emailAddress)
+                .selectGender(userTestData.gender)
+                .setPhone(userTestData.phoneNumber)
                 .submitButtonClick();
-        assertTitle(TITLE_TEXT);
+        assertTitle(UserTestData.TITLE_TEXT);
     }
 }
--- src/test/java/utils/RandomUtils.java
+++ src/test/java/utils/RandomUtils.java
@@ -1,30 +1,35 @@
 package utils;
 
+import com.github.javafaker.Faker;
+
+import java.util.Locale;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomUtils {
-    public static int getRandomInt(int min , int max) {
+    public Faker faker = new Faker();
+
+    public static int getRandomInt(int min, int max) {
         return ThreadLocalRandom.current().nextInt(min, max + 1);
     }
 
     public String getGender() {
         String[] array = {"Male", "Female", "Other"};
-        return array[getRandomInt(0, array.length - 1)];
+        return faker.options().option(array);
     }
 
     public String getHobby() {
         String[] array = {"Sports", "Reading", "Music"};
-        return array[getRandomInt(0, array.length - 1)];
+        return faker.options().option(array);
     }
 
     public String getPhoneNumber() {
-        return String.format("%s%s%s", getRandomInt(900, 999), getRandomInt(111, 999), getRandomInt(1111, 9999));
+        return faker.phoneNumber().subscriberNumber(10);
     }
 
     public String getMonth() {
         String[] array = {"January", "February", "March", "April", "May", "June", "July", "August", "September",
-        "October", "November", "December"};
-        return array[getRandomInt(0, array.length - 1)];
+                "October", "November", "December"};
+        return faker.options().option(array);
     }
 
     public String getYear(int fromYear, int toYear) {
@@ -33,26 +38,26 @@ public class RandomUtils {
 
     public String getState() {
         String[] array = {"NCR", "Uttar Pradesh", "Haryana", "Rajasthan"};
-        return array[getRandomInt(0, array.length - 1)];
+        return faker.options().option(array);
     }
 
     public String getCity(String state) {
         return switch (state) {
             case ("NCR") -> {
                 String[] arrayNCR = {"Delhi", "Gurgaon", "Noida"};
-                yield arrayNCR[getRandomInt(0, arrayNCR.length - 1)];
+                yield faker.options().option(arrayNCR);
             }
             case ("Uttar Pradesh") -> {
                 String[] arrayUttar = {"Agra", "Lucknow", "Merrut"};
-                yield arrayUttar[getRandomInt(0, arrayUttar.length - 1)];
+                yield faker.options().option(arrayUttar);
             }
             case ("Haryana") -> {
                 String[] arrayHaryana = {"Karnal", "Panipat"};
-                yield arrayHaryana[getRandomInt(0, arrayHaryana.length - 1)];
+                yield faker.options().option(arrayHaryana);
             }
             case ("Rajasthan") -> {
                 String[] arrayRajasthan = {"Jaipur", "Jaiselmer"};
-                yield arrayRajasthan[getRandomInt(0, arrayRajasthan.length - 1)];
+                yield faker.options().option(arrayRajasthan);
             }
             default -> "No found";
         };
